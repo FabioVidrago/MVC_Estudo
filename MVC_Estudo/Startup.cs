9c6bff33fd8c3900625e84dde376b2a4096d1243
FabioVidrago <vidragosvsky@hotmail.com>
2023-09-21 11:21:08 +0100

    aletração do tema

--- MVC_Estudo/Startup.cs
+++ MVC_Estudo/Startup.cs
@@ -37,7 +37,7 @@ namespace MVC_Estudo
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<MVC_EstudoContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("MVC_EstudoContext")));
+                    options.UseMySql(Configuration.GetConnectionString("MVC_EstudoContext"),builder => builder.MigrationsAssembly("MVC_Estudo")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
