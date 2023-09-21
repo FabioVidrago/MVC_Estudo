60175b58addb184a10d93b62ca3bfbc17968886f
FabioVidrago <vidragosvsky@hotmail.com>
2023-09-21 12:36:10 +0100

    segunda migarção e adicionada novas entidades

--- MVC_Estudo/Data/MVC_EstudoContext.cs
+++ MVC_Estudo/Data/MVC_EstudoContext.cs
@@ -14,6 +14,8 @@ namespace MVC_Estudo.Data
         {
         }
 
-        public DbSet<MVC_Estudo.Models.Department> Department { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
     }
 }
--- MVC_Estudo/Migrations/20230921113445_OtherEntities.Designer.cs
+++ MVC_Estudo/Migrations/20230921113445_OtherEntities.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using MVC_Estudo.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace MVC_Estudo.Migrations
 {
     [DbContext(typeof(MVC_EstudoContext))]
-    partial class MVC_EstudoContextModelSnapshot : ModelSnapshot
+    [Migration("20230921113445_OtherEntities")]
+    partial class OtherEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
